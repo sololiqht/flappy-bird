523f2049d6094090b5586be94cd064958dc4f98a
sololiqht <cgxhacker888@gmail.com>
2024-10-30 11:17:48 +0500

    bird comp

--- bird.py
+++ bird.py
@@ -1,5 +1,4 @@
 import pygame as pg
-import sys , time
 
 class Bird(pg.sprite.Sprite):
     def __init__(self,scale_factor):
@@ -11,7 +10,43 @@ class Bird(pg.sprite.Sprite):
         self.rect = self.image.get_rect(center = (100,100))
         self.y_velocity = 0
         self.gravity = 10
+        self.flap_speed = 250
+        self.anim_counter = 0
+        self.update_on = False
 
     def update(self,dt):
-        self.y_velocity += self.gravity
+        if self.update_on:
+            self.playAnimation()
+            self.applygravity(dt)
+
+            if self.rect.y<=0 and self.flap_speed==250:
+                self.rect.y=0
+                self.flap_speed=0
+                self.y_velocity=0
+            elif self.rect.y>0 and self.flap_speed==0:
+                self.flap_speed=250
+
+         
+    def applygravity(self,dt):
+        self.y_velocity+=self.gravity*dt
+        self.rect.y+=self.y_velocity
+
+    def flap(self,dt):
+        self.y_velocity=-self.flap_speed*dt
+
+    def playAnimation(self):
+        if self.anim_counter==5:
+            self.image=self.img_list[self.image_index]
+            if self.image_index==0: self.image_index=1
+            else: self.image_index=0
+            self.anim_counter=0
+        
+        self.anim_counter+=1
+
+    def resetposition(self):
+        self.rect.center=(100,100)
+        self.y_velocity=0
+        self.anim_counter=0
+
+
     
